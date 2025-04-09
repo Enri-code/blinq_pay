0c4777a4fbac074cf46c0f436edc597bd8baf4c0
Eric Onyeulo <onyeuloeric@gmail.com>
2025-04-09 17:10:35 +0100

    feat: updated theeme colors

--- lib/core/theme/dark_theme.dart
+++ lib/core/theme/dark_theme.dart
@@ -5,8 +5,11 @@ final darkTheme = ThemeData.dark().copyWith(
     backgroundColor: Colors.black,
     foregroundColor: Colors.white,
   ),
+  shadowColor: Colors.black.withAlpha(60),
   scaffoldBackgroundColor: Colors.black,
   primaryColorLight: const Color(0xFFEB7E3D),
   primaryColorDark: const Color(0xFF7E41D6),
+  primaryColor: Colors.white,
+  tabBarTheme: const TabBarTheme(labelColor: Colors.white),
   // scaffoldBackgroundColor: Color(0xFF121212),
 );
--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -5,7 +5,10 @@ final lightTheme = ThemeData.light().copyWith(
     backgroundColor: Colors.white,
     foregroundColor: Colors.black,
   ),
+  shadowColor: Colors.black.withAlpha(60),
   scaffoldBackgroundColor: Colors.white,
   primaryColorLight: const Color(0xFFEB7E3D),
   primaryColorDark: const Color(0xFF7E41D6),
+  primaryColor: Colors.black,
+  tabBarTheme: const TabBarTheme(labelColor: Color(0xFF7E41D6)),
 );
