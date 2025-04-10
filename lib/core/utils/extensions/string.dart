30fe79882caed3e5726622940abb6cd362acdad7
Eric Onyeulo <onyeuloeric@gmail.com>
2025-04-10 23:29:31 +0100

    fix: string extension  fixed for empty string cases

--- lib/core/utils/extensions/string.dart
+++ lib/core/utils/extensions/string.dart
@@ -1,13 +1,20 @@
 extension StringExt on String {
-  String get firstName => split(' ').firstOrNull ?? '';
+  String get firstName {
+    if (isEmpty) return '';
+    return split(' ').firstOrNull ?? '';
+  }
 
   String get initials {
-    final names = split(' ');
-    if (names.length == 1) {
-      return names.first.substring(0, 1).toUpperCase();
-    } else if (names.length > 1) {
-      return '${names.first.substring(0, 1).toUpperCase()}${names.last.substring(0, 1).toUpperCase()}';
+    if (isEmpty) {
+      return '';
+    } else {
+      final names = split(' ');
+      if (names.length == 1) {
+        return names.first.substring(0, 1).toUpperCase();
+      } else {
+        return '${names.first.substring(0, 1).toUpperCase()}'
+            '${names.last.substring(0, 1).toUpperCase()}';
+      }
     }
-    return '';
   }
 }
